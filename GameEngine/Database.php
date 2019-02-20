8b0146f6ac5f5b5cc7746939820fac1e4c6c858f
Pietro <iopietro@hotmail.it>
2019-02-20 11:48:59 +0100

    Added TB_PREFIX

--- GameEngine/Database.php
+++ GameEngine/Database.php
@@ -787,9 +787,9 @@ class MYSQLi_DB implements IDbConnection {
 	public function hasBeginnerProtection($vid) {
         list($vid) = $this->escape_input((int) $vid);
         $q = "SELECT u.protect 
-              FROM s1_users u 
-              JOIN s1_vdata v ON u.id = v.owner
-              JOIN s1_odata o ON u.id = o.owner
+              FROM ". TB_PREFIX ."_users u 
+              JOIN ". TB_PREFIX ."_vdata v ON u.id = v.owner
+              JOIN ". TB_PREFIX ."_odata o ON u.id = o.owner
               WHERE v.wref = ". $vid ." OR o.wref = ". $vid . " 
               LIMIT 1";
 		$result = mysqli_query($this->dblink,$q);
